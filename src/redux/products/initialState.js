be7eca2548572fc620db0cea40c4ec70baa5a263
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-02 20:43:23 +0600

    feat(products): create initial state for product

---
+++ src/redux/products/initialState.js
@@ -0,0 +1,18 @@
+export const initialState = [
+  {
+    id: "1",
+    name: "Walton Monitor",
+    category: "Technology",
+    image: "https://placehold.co/300",
+    price: 71100,
+    quantity: 6,
+  },
+  {
+    id: "2",
+    name: "Asus Monitor",
+    category: "Technology",
+    image: "https://placehold.co/300",
+    price: 38500,
+    quantity: 3,
+  },
+];
