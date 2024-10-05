e2e079040cce9eca1f0702b491e3fc01cdbabcd4
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-05 21:42:19 +0600

    feat(cartsReducer): update reducer to handle adding items and updating quantities

--- src/redux/carts/cartsReducer.js
+++ src/redux/carts/cartsReducer.js
@@ -1,17 +1,25 @@
-import { CART_ADDED } from "./actionType";
+import { ADD_TO_CART } from "./actionType";
 
+const isExist = (state, action) =>
+  state.some((item) => item.id === action.payload.id);
 
 export const initialState = [];
 
 export const cartsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case CART_ADDED:
-      return [
-        ...state,
-        {
-          ...action.payload,
-        },
-      ];
+    case ADD_TO_CART:
+      if (isExist(state, action)) {
+        if (action.payload.quantity <= 0) {
+          return state;
+        }
+      }
+      return state.map((item) => {
+        return {
+          ...item,
+          cartQuantity: item.cartQuantity + 1,
+          quantity: item.quantity - 1,
+        };
+      });
     default:
       return state;
   }
