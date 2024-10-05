88a29cc9f43f250b54c3a4e7eb06cbb89d0c3458
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-05 19:13:46 +0600

    feat(actionCretors): add addProductQuantity action creator

--- src/redux/products/actionCreators.js
+++ src/redux/products/actionCreators.js
@@ -1,4 +1,5 @@
 import {
+  ADD_MANY_QUANTITY,
   ADD_PRODUCT,
   ADD_PRODUCT_QUANTITY,
   REMOVE_PRODUCT_QUANTITY,
@@ -21,6 +22,13 @@ export const addProductQuantity = (product) => {
 export const removeProductQuantity = (product) => {
   return {
     type: REMOVE_PRODUCT_QUANTITY,
+    payload: product,
+  };
+};
+
+export const addManyQuantity = (product) => {
+  return {
+    type: ADD_MANY_QUANTITY,
     payload: product,
   };
 };
