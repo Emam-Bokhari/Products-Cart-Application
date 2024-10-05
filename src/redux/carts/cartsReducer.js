7b0c805343181990d737a56464c55f1143a15372
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-05 21:58:35 +0600

    feat(cartsReducer): improve reducer to handle adding and removing items with quantity management

--- src/redux/carts/cartsReducer.js
+++ src/redux/carts/cartsReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "./actionType";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "./actionType";
 
 const isExist = (state, action) =>
   state.some((item) => item.id === action.payload.id);
@@ -12,15 +12,42 @@ export const cartsReducer = (state = initialState, action) => {
         if (action.payload.quantity <= 0) {
           return state;
         }
+        return state.map((item) => {
+          if (item.id === action.payload.id) {
+            return {
+              ...item,
+              cartQuantity: item.cartQuantity + 1,
+              quantity: item.quantity - 1,
+            };
+          }
+          return item; 
+        });
       }
-      return state.map((item) => {
-        return {
-          ...item,
-          cartQuantity: item.cartQuantity + 1,
-          quantity: item.quantity - 1,
-        };
-      });
+
+      return [...state, { ...action.payload, cartQuantity: 1 }];
+
+    case REMOVE_FROM_CART: {
+      const currentItem = state.find((item) => item.id === action.payload.id);
+      if (currentItem) {
+        if (currentItem.cartQuantity > 1) {
+          return state.map((item) => {
+            if (item.id === action.payload.id) {
+              return {
+                ...item,
+                cartQuantity: item.cartQuantity - 1,
+                quantity: item.quantity + 1,
+              };
+            }
+            return item; 
+          });
+        }
+        
+        return state.filter((item) => item.id !== action.payload.id);
+      }
+      return state; 
+    }
+
     default:
-      return state;
+      return state; 
   }
 };
